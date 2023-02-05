c8733b8592adaaf9112c3ae3a7cbf2a9678dd806
Jonas de Souza Beserra <joonascontato@gmail.com>
2023-02-05 17:22:40 -0300

    test: user

---
+++ test/unit/domain/entities/user.test.ts
@@ -0,0 +1,22 @@
+
+import {describe,test,expect,beforeAll,afterEach} from 'vitest'
+import { faker } from '@faker-js/faker';
+import User from '../../../../src/domain/entities/user';
+import Email from '../../../../src/domain/email';
+import Password from '../../../../src/domain/password';
+import Name from '../../../../src/domain/name';
+import Username from '../../../../src/domain/username';
+
+describe("User", () => {
+    test("Deve criar um usuário", () => {
+        const fakeEmail = faker.internet.email()
+        const fakePassword = '12aSx#'
+        const user = new User(new Email(fakeEmail), new Password(fakePassword), new Name('teste'), new Username('testeUsername'))
+        const userObject = user.getUser()
+        expect(userObject).toMatchObject({
+            name: "teste",
+            username: "testeUsername",
+            email: fakeEmail,
+        })
+    })
+})
