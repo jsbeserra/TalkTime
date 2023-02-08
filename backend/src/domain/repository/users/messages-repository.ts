5409dccdfcbe04e5efee7aaa72c41a840c3d56f6
jonas <jsbeserra@Stefanini.com>
2023-02-08 18:05:41 -0300

    feat: add message repository

---
+++ backend/src/domain/repository/users/messages-repository.ts
@@ -0,0 +1,6 @@
+import Message from "../../entities/message"
+
+export interface MessagesRepository {
+    save(message:Message): Promise<void>
+    getMessages(senderUsername: string, recipientUsername: string): Promise<Message[]>
+}
