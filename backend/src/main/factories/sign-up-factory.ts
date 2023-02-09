d77502cae213fd5946991112afcae3c480bb13e4
jonas <jsbeserra@Stefanini.com>
2023-02-09 15:13:34 -0300

    refactor: move SignUpController to presentation folder

--- backend/src/main/factories/sign-up-factory.ts
+++ backend/src/main/factories/sign-up-factory.ts
@@ -6,7 +6,7 @@ import ConnectionMongoDb from "src/infra/connection/connectionMongoDb"
 import { WebController } from "src/infra/http/web-controller"
 import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
 import { environment } from "../config/config"
-import { SignUpController } from "../controllers/sign-up"
+import { SignUpController } from "../../presentation/controllers/sign-up"
 
 export const makeSignUpController = (): WebController => {
     const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
