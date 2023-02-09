f0556a27fb2798597923e477f9a40d9abef481ac
jonas <jsbeserra@Stefanini.com>
2023-02-09 15:45:16 -0300

    feat: add factory makeSignUpController

--- backend/src/main/routes/sign.ts
+++ backend/src/main/routes/sign.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { adaptRoute } from "src/infra/http/express/express-route-adapter"
 import { makeSignInController } from "../factories/sign-in-factory"
+import { makeSignUpController } from "../factories/sign-up-factory"
 
 export default (router: Router): void => {
-    router.get('/sign-up', adaptRoute(makeSignInController()))
+    router.get('/sign-ip', adaptRoute(makeSignInController()))
+    router.get('/sign-up', adaptRoute(makeSignUpController()))
 }
