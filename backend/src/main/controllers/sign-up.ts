dc2c43d46128bd87b981b7de19520f81014e500c
jonas <jsbeserra@Stefanini.com>
2023-02-09 14:28:49 -0300

    feat: add SignUpController

---
+++ backend/src/main/controllers/sign-up.ts
@@ -0,0 +1,22 @@
+import { UseCase } from "src/aplication/use-case/use-case";
+import { ControllerOperation, HttpRequest, HttpResponse } from "src/infra/http/ports";
+import { badRequest, ok } from "src/infra/http/util";
+
+export class SignUpController implements ControllerOperation {
+    readonly requiredParams: string[] = ['email', 'password', 'name','username'];
+    private useCase: UseCase;
+
+    constructor(useCase: UseCase) {
+        this.useCase = useCase
+    }
+
+    async operation(request: HttpRequest): Promise<HttpResponse> {
+        try {
+            const result = await this.useCase.exec(request.params)
+            return ok(result)
+        } catch (err: any) {
+            return badRequest(err.message)
+        }
+    }
+
+}
