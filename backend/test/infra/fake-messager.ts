3b62832ee17d5aff59e77312f4111e6c87722796
jonas <jsbeserra@Stefanini.com>
2023-02-15 14:40:54 -0300

    test: add FakeMessager for tests

--- backend/test/infra/fake-messager.ts
+++ backend/test/infra/fake-messager.ts
@@ -1,7 +1,11 @@
 import Messenger from "src/domain/messenger";
 
 export default class FakeMessager implements Messenger{
+
     async deliver(recipient: string, message: string): Promise<void> {
         
-    }   
+    }  
+    async collect(callback): Promise<void> {
+
+    } 
 }
