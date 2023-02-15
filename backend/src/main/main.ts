9e464d8ba90a99b5ab8d8856aff28dfc68e7dc2d
jonas <jsbeserra@Stefanini.com>
2023-02-15 14:45:56 -0300

    feat: implements socket io in main.ts

--- backend/src/main/main.ts
+++ backend/src/main/main.ts
@@ -1,8 +1,13 @@
+import SocketIoAdpter from "src/infra/adpters/socketio-adpter";
 import ExpressHttpServer from "../infra/http/express/ExpressHttpServer";
+import { makeSendMessage } from "./factories/send-message-factory";
 
 async function main() {
 	const httpServer = new ExpressHttpServer();
 	httpServer.listen(3003);
+	const socketIoAdpter = new SocketIoAdpter(httpServer.app)
+	const sendMessage = makeSendMessage(socketIoAdpter)
+	socketIoAdpter.collect(sendMessage)
 	console.log("Server started")
 }
 
