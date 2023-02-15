029c69a5503564c1ec463865b57ccabd1f345ceb
jonas <jsbeserra@Stefanini.com>
2023-02-15 11:52:04 -0300

    feat: remove events

--- backend/src/domain/Events.ts
+++
@@ -1,5 +0,0 @@
-export interface Events {
-    on(event:string,callback: () => any): Promise<void>
-    listen(event:string,callback: () => any): Promise<void>
-    send(event:string,callback: () => any): Promise<void>
-}
