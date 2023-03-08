e462ffa782ee6ce1a101dd1f6d9e18ce0a01a47f
jonas <jsbeserra@Stefanini.com>
2023-03-08 16:59:39 -0300

    feat: SingIn add event onsubmit

--- frontend/src/presentation/pages/sing-in/components/sing-in.tsx
+++ frontend/src/presentation/pages/sing-in/components/sing-in.tsx
@@ -1,18 +1,40 @@
-import { Grid, Input, HStack, VStack, Flex, Button, FormControl, FormLabel, Image, Card, Heading, Container, Divider } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React from 'react';
+import { Input, VStack, Flex, Button, FormControl, FormLabel, Heading, Divider, useToast,Text } from '@chakra-ui/react';
 import SignInUseCase from '../../../../aplication/usecase/sign-in/sign-in';
-
+import { object, string } from 'yup';
+import { useFormik } from 'formik';
 interface ISingIn {
     signInUseCase: SignInUseCase
 }
 
+let userSchema = object().shape({
+    email: string().email().required('Campo obrigatório'),
+    password: string().min(6, 'Minímo de 6 letras').required('Campo obrigatório'),
+});
+
 const SingIn: React.FC<ISingIn> = ({ signInUseCase }) => {
-    const [email, setemail] = useState<string>("")
-    const [password, setpassword] = useState<string>("")
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        alert(`Email: ${email} & Password: ${password}`);
-    };
+    const toast = useToast()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: userSchema,
+        onSubmit: async (values) => {
+            const result = await signInUseCase.handle(values)
+            if (result.isLeft()) {
+                toast({
+                    title: 'Erro',
+                    description: result.value.message,
+                    position: 'top-right',
+                    status: 'error',
+                    duration: 2000,
+                    isClosable: true,
+                })
+            }
+        },
+    });
+
 
     return (
         <VStack w={'50%'} spacing={4} justifyContent='center' alignItems={'center'} p='10'>
@@ -26,15 +48,17 @@ const SingIn: React.FC<ISingIn> = ({ signInUseCase }) => {
                         Entrar
                     </Heading>
                 </VStack>
-                <form onSubmit={handleSubmit} style={{ width: '100%' }}>
+                <form onSubmit={formik.handleSubmit} style={{ width: '100%' }}>
                     <VStack spacing='6' w='100%'>
                         <FormControl isRequired>
                             <FormLabel>Email</FormLabel>
-                            <Input placeholder="Email" value={email} onChange={event => setemail(event.currentTarget.value)} />
+                            <Input name='email' placeholder="Email" onChange={formik.handleChange} />
+                            {formik.errors.email ? <Text variant={'error'}>{formik.errors.email}</Text> : null}
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Password</FormLabel>
-                            <Input type='password' placeholder="Password" value={password} onChange={event => setpassword(event.currentTarget.value)} />
+                            <Input name='password' type='password' placeholder="Password" onChange={formik.handleChange} />
+                            {formik.errors.password ? <Text variant={'error'}>{formik.errors.password}</Text> : null}
                         </FormControl>
                         <Button type='submit' variant={'default'} w='100%'>Login</Button>
                     </VStack>
