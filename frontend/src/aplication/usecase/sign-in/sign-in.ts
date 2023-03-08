f8add8e72f959231ed14b56d7a529e5ccf2c4a3a
jonas <jsbeserra@Stefanini.com>
2023-03-08 16:58:25 -0300

    feat: SignInUseCase add heither

--- frontend/src/aplication/usecase/sign-in/sign-in.ts
+++ frontend/src/aplication/usecase/sign-in/sign-in.ts
@@ -1,16 +1,19 @@
 import { ISignGateway } from "../../../domain/gateways/sing/sign-gateway";
+import { Either, left, right } from "../../../shared/either";
+import ResponseError from "../../../shared/response-error";
 import { InputSignIn } from "./input-sign-in";
 
 export default class SignInUseCase {
 
     constructor(private signGateway: ISignGateway) { }
 
-    async handle(input: InputSignIn): Promise<void> {
+    async handle(input: InputSignIn): Promise<Either<ResponseError, void>> {
         const account = await this.signGateway.singIn(input.email, input.password)
-        if(account.isLeft()) return
+        if(account.isLeft()) return left(account.value)
         sessionStorage.setItem('jwt', account.value.jwt)
         sessionStorage.setItem('name', account.value.name)
         sessionStorage.setItem('username', account.value.username)
         sessionStorage.setItem('email', account.value.email)
+        return right(undefined);
     }
 }
