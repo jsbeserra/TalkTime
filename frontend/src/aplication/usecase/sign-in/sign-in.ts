3229fce9ceb14d59d97a59ff19a40861df2316e0
jonas <jsbeserra@Stefanini.com>
2023-03-09 09:37:50 -0300

    refactor: rename jwt for token

--- frontend/src/aplication/usecase/sign-in/sign-in.ts
+++ frontend/src/aplication/usecase/sign-in/sign-in.ts
@@ -10,7 +10,8 @@ export default class SignInUseCase {
     async handle(input: InputSignIn): Promise<Either<ResponseError, void>> {
         const account = await this.signGateway.singIn(input.email, input.password)
         if(account.isLeft()) return left(account.value)
-        sessionStorage.setItem('jwt', account.value.jwt)
+        console.log(account.value)
+        sessionStorage.setItem('token', account.value.token)
         sessionStorage.setItem('name', account.value.name)
         sessionStorage.setItem('username', account.value.username)
         sessionStorage.setItem('email', account.value.email)
--- frontend/src/infra/gateways/sign/sign-gateway.ts
+++ frontend/src/infra/gateways/sign/sign-gateway.ts
@@ -11,7 +11,7 @@ export default class SignGateway implements ISignGateway {
     async singIn(email: string, password: string): Promise<Either<ResponseError, Account>> {
         const result = await this.httpClient.post('/sign-in', { email, password })
         if(result.isLeft()) return left(new ResponseError(result.value.message,result.value.statusCode))
-        return right(new Account(result.value.username, result.value.name, result.value.email, result.value.jwt))
+        return right(new Account(result.value.username, result.value.name, result.value.email, result.value.token))
     }
 
     async singUp(input: inputSingUp): Promise<Either<ResponseError, singUpOutPut>> {
