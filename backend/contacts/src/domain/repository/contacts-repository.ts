60111b953299894dbd16e63ff2ab4a74e8976a09
jonas <jsbeserra@Stefanini.com>
2023-03-14 11:34:46 -0300

    feat: add contacts repository

---
+++ backend/contacts/src/domain/repository/contacts-repository.ts
@@ -0,0 +1,6 @@
+import Contacts from "../entities/contacts"
+
+export interface ContactsRepository {
+    find(ownerUsername: string): Promise<Contacts | undefined>
+    add(ownerUsername: string, contactUsername: string): Promise<void>
+}
---
+++ backend/contacts/src/infra/repository/contacts-repository-mongo.ts
@@ -0,0 +1,37 @@
+import Contacts from "../../domain/entities/contacts";
+import { ContactsRepository } from "../../domain/repository/contacts-repository";
+import ConnectionMongoDb from "../connection/connectionMongoDb";
+
+export default class ContactsRepositoryMongo implements ContactsRepository {
+
+    constructor(readonly connectionMongoDb: ConnectionMongoDb) { }
+
+    async find(ownerUsername: string): Promise<Contacts | undefined> {
+        const collection = await this.connectionMongoDb.getCollection('contacts')
+        const result = await collection.findOne({ ownerUsername: ownerUsername })
+        const contacts = new Contacts(ownerUsername)
+        for(const user of result.contacts){
+            contacts.addContact(user.userName)
+        }
+        return contacts
+    }
+
+    async add(ownerUsername: string, contactUsername: string): Promise<void> {
+        const collection = await this.connectionMongoDb.getCollection('contacts')
+        const result = await collection.findOne({ ownerUsername: ownerUsername })
+        if(result){
+            await collection.updateOne({ownerUsername: ownerUsername},{
+                $set:{
+                    contacts:{contactUsername}
+                }
+            })
+        }else{
+            await collection.insertOne({
+                ownerUsername: ownerUsername,
+                $set:{
+                    contacts:{contactUsername}
+                }
+            })
+        }
+    }
+}
