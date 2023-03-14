b5478202b948bfa32c5123776299186396adcde2
jonas <jsbeserra@Stefanini.com>
2023-03-14 11:35:04 -0300

    feat: add Contacts

---
+++ backend/contacts/src/domain/entities/contacts.ts
@@ -0,0 +1,13 @@
+
+export default class Contacts {
+    private _userNames: string[] = []
+    constructor(readonly ownerUsername:string){}
+
+    public addContact(userName:string){
+        this._userNames.push(userName)
+    }
+
+    get contacts(){
+        return this._userNames
+    }
+}
