446de17222eda6efb39c8bbb514be4f2ea829630
jonas <jsbeserra@Stefanini.com>
2023-03-22 21:59:00 -0300

    feat: FindUserByUsername import correction FindUserByEmail

--- backend/contacts/src/main/factories/find-user-by-username-factory.ts
+++ backend/contacts/src/main/factories/find-user-by-username-factory.ts
@@ -3,7 +3,7 @@ import { WebController } from "src/infra/http/web-controller"
 import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
 import { environment } from "../config/config"
 import { FindUserByUsernameController } from "src/presentation/controllers/find-user-by-username"
-import FindUserByUsername from "src/aplication/use-case/find-users-by-username/FindUserByUsername"
+import FindUserByUsername from "src/aplication/use-case/find-users-by-username/find-user-by-username"
 
 export const makeFindUserByUsernameController = (): WebController => {
     const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
