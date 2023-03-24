474da6dd736986504009eea98012165016405888
jonas <jsbeserra@Stefanini.com>
2023-03-24 17:46:41 -0300

    fix: makeSignUpController import SingUpAddUseCase

--- backend/authentication/src/main/factories/sign-up-factory.ts
+++ backend/authentication/src/main/factories/sign-up-factory.ts
@@ -4,13 +4,13 @@ import { WebController } from "src/infra/http/web-controller"
 import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
 import { environment } from "../config/config"
 import { SignUpController } from "../../presentation/controllers/sign-up"
-import UserAddUseCase from "src/aplication/use-case/user/add-user/user-add-usecase"
+import SingUpAddUseCase from "src/aplication/use-case/sing-up/sing-up-usecase"
 
 export const makeSignUpController = (): WebController => {
     const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
     const userRepository = new UserRepositoryMongo(mongoDbDriveAdpter)
     const encoderAdpterBcrypt = new EncoderAdpterBcrypt()
-    const usecase = new UserAddUseCase(userRepository,encoderAdpterBcrypt)
+    const usecase = new SingUpAddUseCase(userRepository,encoderAdpterBcrypt)
     const controller = new WebController(new SignUpController(usecase))
     return controller
 }
