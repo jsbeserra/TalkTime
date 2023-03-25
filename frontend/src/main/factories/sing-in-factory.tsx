d95b22b2a49b1b884963d76c3c776129a63e9271
jonas <jsbeserra@Stefanini.com>
2023-03-25 11:01:47 -0300

    feat: environment add API_CONTACTS_URL

--- frontend/src/main/factories/sing-in-factory.tsx
+++ frontend/src/main/factories/sing-in-factory.tsx
@@ -3,9 +3,10 @@ import SignInUseCase from '../../aplication/usecase/sign-in/sign-in';
 import SignGateway from '../../infra/gateways/sign/sign-gateway';
 import AxiosAdapter from '../../infra/http/http-axios-adpter';
 import SingIn from '../../presentation/pages/sing-in/components/sing-in';
+import { environment } from '../config/config';
 
 const MakeSingIn: React.FC = () => {
-  const axiosAdapter =  new AxiosAdapter()
+  const axiosAdapter =  new AxiosAdapter(environment.API_AUTH)
   const signGateway = new SignGateway(axiosAdapter)
   const signInUseCase = new SignInUseCase(signGateway)
   return <SingIn signInUseCase={signInUseCase} />;
