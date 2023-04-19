2c026f7384d9be996479db32f6910feb80468b70
jonas <jsbeserra@Stefanini.com>
2023-04-19 16:25:31 -0300

    feat: add makeSendInviteUseCase

---
+++ backend/contacts/src/main/factories/send-invite-factory.ts
@@ -0,0 +1,14 @@
+import ConnectionMongoDb from "src/infra/connection/connectionMongoDb"
+import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
+import { environment } from "../config/config"
+import SendInvite from "src/aplication/use-case/send-invite/send-invite"
+import InviteRepositoryMongoDb from "src/infra/repository/invite-repository"
+
+
+export const makeSendInviteUseCase = () => {
+    const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
+    const userRepository = new UserRepositoryMongo(mongoDbDriveAdpter)
+    const inviteRepository = new InviteRepositoryMongoDb(mongoDbDriveAdpter)
+    const usecase = new SendInvite(userRepository,inviteRepository)
+    return usecase
+}
