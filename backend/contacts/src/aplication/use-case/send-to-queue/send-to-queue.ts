db0f1597ee525a53ba3a84baf772d327d419e41d
jonas <jsbeserra@Stefanini.com>
2023-04-19 16:27:29 -0300

    feat: add SendToQueue use-case

---
+++ backend/contacts/src/aplication/use-case/send-to-queue/send-to-queue.ts
@@ -0,0 +1,10 @@
+import { Queue } from "src/infra/queue/queue";
+import { UseCase } from "../use-case";
+
+export default class SendToQueue implements UseCase{
+    constructor(private queue:Queue, private queueName:string){}
+
+    async handle(request: any): Promise<any> {
+        await this.queue.publish(this.queueName,request)
+    }
+}
