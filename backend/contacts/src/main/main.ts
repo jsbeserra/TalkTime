60fbf9ca79088fb0a0a0268c22d4fc1cdea443f1
jonas <jsbeserra@Stefanini.com>
2023-04-19 16:28:19 -0300

    feat: add makeInviteConsumerUseCase in to main

--- backend/contacts/src/main/main.ts
+++ backend/contacts/src/main/main.ts
@@ -1,13 +1,15 @@
 import http from "http";
 import ExpressHttpServer from "../infra/http/express/ExpressHttpServer";
 import 'dotenv/config';
+import { makeInviteConsumerUseCase } from "./factories/invite-consumer-factory";
 
 async function main() {
 	const httpServerExpress = new ExpressHttpServer();
 	const httpServer = http.createServer(httpServerExpress.app)
 	httpServer.listen(process.env.PORT,()=>{
 		console.log("Server started")
-	})	
+	})
+	makeInviteConsumerUseCase()	
 }
 
 main();
