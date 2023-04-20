1b7e6ebc3f966cb0eefc29fd6ac5e95e5e19fcb4
jonas <jsbeserra@Stefanini.com>
2023-04-20 14:46:55 -0300

    feat: makeFindUserController factory added InviteRepositoryMongoDb

--- backend/contacts/src/main/factories/find-users-factory.ts
+++ backend/contacts/src/main/factories/find-users-factory.ts
@@ -5,13 +5,15 @@ import { environment } from "../config/config"
 import { FindUsersController } from "src/presentation/controllers/find-users"
 import FindUsers from "src/aplication/use-case/find-users/find-user"
 import ContactsRepositoryMongo from "src/infra/repository/contacts-repository-mongo"
+import InviteRepositoryMongoDb from "src/infra/repository/invite-repository"
 
 
 export const makeFindUserController = (): WebController => {
     const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
     const userRepository = new UserRepositoryMongo(mongoDbDriveAdpter)
     const contactsRepository = new ContactsRepositoryMongo(mongoDbDriveAdpter)
-    const usecase = new FindUsers(userRepository,contactsRepository)
+    const inviteRepository = new InviteRepositoryMongoDb(mongoDbDriveAdpter)
+    const usecase = new FindUsers(userRepository,contactsRepository,inviteRepository)
     const controller = new WebController(new FindUsersController(usecase))
     return controller
 }
