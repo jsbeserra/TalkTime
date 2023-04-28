f08c014dc9a4b5fea9d4fd3ccd8bea9bd133b1b1
jsbeserra <joonassouza55@gmail.com>
2023-04-28 17:19:40 -0300

    feat: MakeSingIn add SessionStorageAdpter

--- frontend/src/main/factories/sing-in-factory.tsx
+++ frontend/src/main/factories/sing-in-factory.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import SignInUseCase from '../../aplication/usecase/sign-in/sign-in';
-import SignGateway from '../../infra/gateways/sign/sign-gateway';
-import AxiosAdapter from '../../infra/http/http-axios-adpter';
-import SingIn from '../../presentation/pages/sing-in/components/sing-in';
+import SignInUseCase from '@aplication/usecase/sign-in/sign-in';
+import SignGateway from '@infra/gateways/sign/sign-gateway';
+import AxiosAdapter from '@infra/http/http-axios-adpter';
+import SingIn from '@presentation/pages/sing-in/components/sing-in';
 import { environment } from '../config/config';
+import SessionStorageAdpter from '@infra/SessionStorageAdpter';
 
 const MakeSingIn: React.FC = () => {
-  const axiosAdapter =  new AxiosAdapter(environment.API_AUTH)
+  const axiosAdapter = new AxiosAdapter(environment.API_AUTH)
   const signGateway = new SignGateway(axiosAdapter)
-  const signInUseCase = new SignInUseCase(signGateway)
+  const appStorage = new SessionStorageAdpter()
+  const signInUseCase = new SignInUseCase(signGateway, appStorage)
   return <SingIn signInUseCase={signInUseCase} />;
 }
 
