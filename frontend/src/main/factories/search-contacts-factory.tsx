1528d28eabfa8e690562626384de27ee63dc9e16
jsbeserra <joonassouza55@gmail.com>
2023-05-03 14:03:46 -0300

    feat: change name factories MakeFindContacts to MakeSearchContacts

--- frontend/src/main/factories/search-contacts-factory.tsx
+++ frontend/src/main/factories/search-contacts-factory.tsx
@@ -4,12 +4,16 @@ import ContactsGateway from '@infra/gateways/contacts/contacts-gateway'
 import AxiosAdapter from '@infra/http/http-axios-adpter'
 import SearchContacts from '@presentation/components/modals/find-contacts/search-contacts'
 import { environment } from '../config/config'
+import SendInviteContact from '@aplication/usecase/send-invite-contact/send-invite-contact'
+import SessionStorageAdpter from '@infra/SessionStorageAdpter'
 
-const MakeFindContacts: React.FC = () => {
+const MakeSearchContacts: React.FC = () => {
 	const axiosAdapter = new AxiosAdapter(environment.API_CONTACTS_URL)
 	const contactsGateway = new ContactsGateway(axiosAdapter)
 	const findContacts = new FindContacts(contactsGateway)
-	return <SearchContacts findContactsUsecase={findContacts}/>
+	const sendInviteContact = new SendInviteContact(contactsGateway)
+	const sessionStorageAdpter = new SessionStorageAdpter()
+	return <SearchContacts findContactsUsecase={findContacts} inviteContactUsecase={sendInviteContact} appStorage={sessionStorageAdpter}/>
 }
 
-export default MakeFindContacts
+export default MakeSearchContacts
