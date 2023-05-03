91b86a31065821bd525e4cf6299356f1ab017792
jsbeserra <joonassouza55@gmail.com>
2023-05-03 14:00:08 -0300

    fix: makeFindContactController change AddContactController to FindContactsController

--- backend/contacts/src/main/factories/find-contacts-factory.ts
+++ backend/contacts/src/main/factories/find-contacts-factory.ts
@@ -3,14 +3,14 @@ import { WebController } from "src/infra/http/web-controller"
 import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
 import { environment } from "../config/config"
 import ContactsRepositoryMongo from "src/infra/repository/contacts-repository-mongo"
-import { AddContactController } from "src/presentation/controllers/add-contact"
 import FindContatcs from "src/aplication/use-case/find-contacts/find-contacts"
+import { FindContactsController } from "src/presentation/controllers/find-contacts"
 
 export const makeFindContactController = (): WebController => {
     const mongoDbDriveAdpter: ConnectionMongoDb = new ConnectionMongoDb(environment.MONGODB_URI,environment.MONGODB_NAME)
     const userRepository = new UserRepositoryMongo(mongoDbDriveAdpter)
     const contactsRepository = new ContactsRepositoryMongo(mongoDbDriveAdpter)
     const usecase = new FindContatcs(userRepository,contactsRepository)
-    const controller = new WebController(new AddContactController(usecase))
+    const controller = new WebController(new FindContactsController(usecase))
     return controller
 }
