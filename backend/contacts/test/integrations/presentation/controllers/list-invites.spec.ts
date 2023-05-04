876f2faaa166e9d85e5ad199cfe01c85c78d598e
jsbeserra <joonassouza55@gmail.com>
2023-05-04 13:21:10 -0300

    test: add List-invite controller

--- backend/contacts/test/integrations/presentation/controllers/list-invites.spec.ts
+++ backend/contacts/test/integrations/presentation/controllers/list-invites.spec.ts
@@ -1,33 +1,35 @@
 import { MongoMemoryServer } from "mongodb-memory-server"
 import ListInvites from "src/aplication/use-case/list-invites/list-invites"
 import ConnectionMongoDb from "src/infra/connection/connectionMongoDb"
-import ContactsRepositoryMongo from "src/infra/repository/contacts-repository-mongo"
+import { HttpRequest } from "src/infra/http/ports"
+import { WebController } from "src/infra/http/web-controller"
 import InviteRepositoryMongoDb from "src/infra/repository/invite-repository"
 import UserRepositoryMongo from "src/infra/repository/user-repository-mongo"
+import { ListInviteController } from "src/presentation/controllers/list-invite"
 
-describe("ListInvites",()=>{
+
+describe('List-invite controller', () => {
     let userRepository: UserRepositoryMongo
-    let contactsRepository: ContactsRepositoryMongo
-    let inviteRepositoryMongoDb:InviteRepositoryMongoDb
+    let inviteRepository: InviteRepositoryMongoDb
     let connection: ConnectionMongoDb
     let mongod: MongoMemoryServer
-    let sut:ListInvites 
+    let sut: WebController
 
     beforeAll(async () => {
         mongod = await MongoMemoryServer.create();
         const uri = mongod.getUri();
         connection = new ConnectionMongoDb(uri, 'chat_api')
         userRepository = new UserRepositoryMongo(connection)
-        contactsRepository = new ContactsRepositoryMongo(connection)
-        inviteRepositoryMongoDb = new InviteRepositoryMongoDb(connection)
-        sut = new ListInvites(inviteRepositoryMongoDb,userRepository)
+        inviteRepository = new InviteRepositoryMongoDb(connection)
+        let usecase = new ListInvites(inviteRepository,userRepository)
+        sut = new WebController(new ListInviteController(usecase))
     })
 
     afterEach(async () => {
         await (await connection.getCollection('invites')).deleteMany()
     })
 
-    test("Deve Listar os invites do usuário",async ()=>{
+    test('Deve Listar os invites do usuário e retornar status code 200', async () => {
         const collectionUsers = await connection.getCollection('users')
         const userInputData1 = {
             email: 'fakeEmail@gmail.com',
@@ -43,15 +45,22 @@ describe("ListInvites",()=>{
             requester_username:'fakeName',
             targuet_username:'fakeUser2',
             accepted:false
-        })  
-        const result = await sut.handle('fakeUser2')
-        expect(result).toMatchObject([{
+        })     
+        const input: HttpRequest = { query: { username: 'fakeUser2'} }
+        const result = await sut.handle(input)
+        expect(result.body).toMatchObject([{
             requester_username:'fakeName',
             targuet_username:'fakeUser2',
         }])
+        expect(result.statusCode).toBe(200)
     })
-    test("Deve Listar os invites do usuário e se não tiver nenhum deve retornar um array vazio",async ()=>{ 
-        const result = await sut.handle('fakeUser2')
-        expect(result).toMatchObject([])
+
+    test('Deve retornar bad request se o usuário não existir', async () => {    
+        const input: HttpRequest = { query: { username: 'fakeUsernameX'} }
+        const result = await sut.handle(input)
+        expect(result.body).toBe("user not found")
+        expect(result.statusCode).toBe(400)
     })
+
+
 })
