d169231830fe3a6c4fcc13ccfd6a55a1ca719176
jsbeserra <joonassouza55@gmail.com>
2023-05-12 17:41:49 -0300

    feat: add RedisAdpter

---
+++ backend/messager/src/infra/adpters/redis-adpter.ts
@@ -0,0 +1,30 @@
+import Redis from 'ioredis'
+import {environment} from 'src/main/config/config'
+import { promisify } from 'util'
+
+export default class RedisAdpter{
+	private redisclient: Redis
+
+	constructor(){
+		this.redisclient = new Redis({
+			host: environment.REDIS_HOST,
+			port: parseInt(environment.REDIS_PORT),
+			password: environment.REDIS_PASSWORD
+		})
+	}
+
+	get(key:string){
+		const syncRedisGet = promisify(this.redisclient.get).bind(this.redisclient)
+		return syncRedisGet(key)
+	}
+
+	set(key:string,value:string){
+		const syncRedisSet = promisify(this.redisclient.set).bind(this.redisclient)
+		return syncRedisSet(key,value)
+	}
+
+	del(key:string){
+		const syncRedisDel = promisify(this.redisclient.del).bind(this.redisclient)
+		return syncRedisDel(key)
+	}
+}
