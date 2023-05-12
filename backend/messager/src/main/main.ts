1e4b15f58b572b955f6b2547ebb161394e0a0ea4
jsbeserra <joonassouza55@gmail.com>
2023-05-12 19:18:18 -0300

    feat: add kafkaAdapter and makeSaveMessagesQueueConsumerUseCase in main

--- backend/messager/src/main/main.ts
+++ backend/messager/src/main/main.ts
@@ -3,13 +3,15 @@ import 'dotenv/config'
 import express from 'express'
 import { createServer } from 'http'
 import { makeSocketIo } from './factories/socketio-factory'
+import KafkaAdpter from 'src/infra/queue/kafka/kafka-adpter'
+import { makeSaveMessagesQueueConsumerUseCase } from './factories/save-messages-queue'
 
 const app = express()
 const httpServer = createServer(app)
-
-const socket = makeSocketIo()
+const kafkaAdpter = new KafkaAdpter(process.env.CLIENTID,[process.env.BROKERS])
+const socket = makeSocketIo(kafkaAdpter)
+makeSaveMessagesQueueConsumerUseCase(kafkaAdpter)
 socket.start(httpServer)
-
 httpServer.listen(process.env.PORT, () => {
 	console.log('Servidor Socket.IO está ouvindo na porta ' + process.env.PORT)
 })
