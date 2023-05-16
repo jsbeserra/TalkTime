5c9fac9530baa138e7eab5b7ade836bedb196a4a
jsbeserra <joonassouza55@gmail.com>
2023-05-15 21:17:31 -0300

    feat: Number of notifications in the side menu

--- frontend/src/presentation/pages/chat/components/side-menu.tsx
+++ frontend/src/presentation/pages/chat/components/side-menu.tsx
@@ -3,7 +3,7 @@ import { FaUsers } from 'react-icons/fa'
 import { AiFillMessage } from 'react-icons/ai'
 import { IoCallSharp,IoExit,IoNotifications } from 'react-icons/io5'
 import { IoMdSettings } from 'react-icons/io'
-import { Button, VStack } from '@chakra-ui/react'
+import { Button, VStack,Text} from '@chakra-ui/react'
 import { useSearchContacts } from '@main/context/search-contacts-context'
 import Exit from '@aplication/usecase/exit/exit'
 import { useAuth } from '@main/context/auth-context'
@@ -15,16 +15,37 @@ interface ISideMenu {
 
 const SideMenu: React.FC<ISideMenu>= ({exit}) => {
 	const {toogle} = useSearchContacts()
-	const {openContentContacts,isOpenContentContacts,isopenNotifications,openNotifications} = useSideMenu()
+	const {openContentContacts,isOpenContentContacts,isopenNotifications,openNotifications,notifications,setNotifications} = useSideMenu()
 	const {authenticate} = useAuth()
 
+	const openNotification = () =>{
+		openNotifications(!isopenNotifications)
+		if (!isopenNotifications) setNotifications(0)
+	}
 	return (
 		<VStack w={'100%'} alignItems={'center'} flexDir='column' justifyContent={'flex-start'}>
 			<Button variant={'icon'} borderRadius={'full'} w={'40px'} h={'40px'} p={0} onClick={()=>openContentContacts(!isOpenContentContacts)}>
 				<AiFillMessage size={25} />
 			</Button>
-			<Button variant={'icon'} borderRadius={'full'} w={'40px'} h={'40px'} p={0} onClick={()=>openNotifications(!isopenNotifications)}>
+			<Button variant={'icon'} borderRadius={'full'} w={'40px'} h={'40px'} p={0} onClick={()=>openNotification()}>
 				<IoNotifications size={25} />
+				<Text 
+					bg={'green.500'}
+					textAlign={'center'}
+					color={'white'}
+					fontSize={10}
+					borderRadius={'full'}
+					position={'absolute'} 
+					left={'20px'}
+					top={'0px'}
+					w={4} 
+					h={4}
+					display={'flex'}
+					justifyContent={'center'}
+					alignItems={'end'}
+				>
+					{notifications}					
+				</Text>
 			</Button>
 			<Button variant={'icon'} borderRadius={'full'} w={'40px'} h={'40px'} p={0} onClick={()=>toogle(true)}>
 				<FaUsers size={25} />
