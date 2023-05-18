72c12e6d924ccc35b6e08b72734a47c0d41e50cb
jsbeserra <joonassouza55@gmail.com>
2023-05-18 16:31:03 -0300

    feat: add ContactsSlice redux

---
+++ frontend/src/infra/adpters/redux/contacts-slice.ts
@@ -0,0 +1,33 @@
+import { userContact } from '@domain/entities/userContact'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+interface contactsState {
+    contacts:userContact[]
+}
+
+const initialState:contactsState = {
+	contacts:[]
+}
+interface inputMessage {
+	email: string,
+	name: string,
+	username: string,
+	id: string
+}
+export const ContactsSlice= createSlice({
+	name:'contacts',
+	initialState,
+	reducers:{
+		addContact:(state,action:PayloadAction<inputMessage>)=>{
+			const {email,name,username,id} = action.payload
+			const exist = state.contacts.find(e=>e.username === username)
+			if (!exist){
+				state.contacts.push({email,id,name,username})
+				return
+			}
+		}
+	}
+})
+
+export default ContactsSlice.reducer
+export const {addContact} = ContactsSlice.actions
