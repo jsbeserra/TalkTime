3e7f359ab9146b411df722046c5a2f4a50a7ed9e
jsbeserra <joonassouza55@gmail.com>
2023-05-26 10:12:41 -0300

    test/refactor: Fixed testicle names and description

--- backend/auth/auth-test/domain/entities/password.test.cs
+++ backend/auth/auth-test/domain/entities/password.test.cs
@@ -10,32 +10,32 @@ public class PasswordTest
         Assert.Equal(password, passwordObj._value);
     }
 
-    [Fact(DisplayName = "Should must not create password with less than 6 characters")]
-    public void ShouldMustNotCreatePasswordWithLessThan6Characters()
+    [Fact(DisplayName = "Should not create password with less than 6 characters")]
+    public void ShouldNotCreatePasswordWithLessThan6Characters()
     {
         string password = "123@M";
         var exception = Assert.Throws<ArgumentException>(() => new Password(password));
         Assert.Equal("Invalid length, password must contain at least 6 characters", exception.Message);
     }
 
-    [Fact(DisplayName = "Should create invalid password if no special characters are provided")]
-    public void ShouldCreateInvalidPasswordIfNoSpecialCharactersAreProvided()
+    [Fact(DisplayName = "Should not create password if no special characters are provided")]
+    public void ShouldNotCreatePasswordIfNoSpecialCharactersAreProvided()
     {
         string password = "123Mudar";
         var exception = Assert.Throws<ArgumentException>(() => new Password(password));
         Assert.Equal("Password must contain at least one special character", exception.Message);
     }
 
-    [Fact(DisplayName = "Should create invalid password if not contain a capital letter")]
-    public void ShouldCreateInvalidPasswordIfNotContainACapitalLetter()
+    [Fact(DisplayName = "Should not create password if not contain a capital letter")]
+    public void ShouldNotCreateInvalidIfNotContainACapitalLetter()
     {
         string password = "123@mudar";
         var exception = Assert.Throws<ArgumentException>(() => new Password(password));
         Assert.Equal("Must contain a capital letter", exception.Message);
     }
 
-    [Fact(DisplayName = "Should create invalid password if not contain a lowercase letter")]
-    public void ShouldCreateInvalidPasswordIfNotContainALowercaseLetter()
+    [Fact(DisplayName = "Should not create invalid password if not contain a lowercase letter")]
+    public void ShouldNotCreatePasswordIfNotContainALowercaseLetter()
     {
         string password = "123@MUDAR";
         var exception = Assert.Throws<ArgumentException>(() => new Password(password));
@@ -43,8 +43,8 @@ public class PasswordTest
     }
 
 
-    [Fact(DisplayName = "Should create invalid password if not contain numbers")]
-    public void ShouldCreateInvalidPasswordIfNotContainNumbers()
+    [Fact(DisplayName = "Should not create password if not contain numbers")]
+    public void ShouldNotCreatePasswordIfNotContainNumbers()
     {
         string password = "aaa@MUDAR";
         var exception = Assert.Throws<ArgumentException>(() => new Password(password));
