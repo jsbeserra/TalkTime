6922adf817c7fafe62ba047dec60656ce64851af
jsbeserra <joonassouza55@gmail.com>
2023-05-31 15:02:36 -0300

    test: Account add salt

--- backend/auth/auth-test/domain/entities/account.test.cs
+++ backend/auth/auth-test/domain/entities/account.test.cs
@@ -8,13 +8,11 @@ public class AccountTest
         Name name = new Name("teste");
         Username username = new Username("teste@");
         Email email = new Email("teste@teste.com");
-        HashPassword hashPassword = new HashPasswordBcryptAdpter();
-        Password password = new Password("1122@xxX",hashPassword);
-        Account accountObj = new Account(name,username,email,password);
+        Password password = new Password("1122@xxX");
+        Account accountObj = new Account(name,username,email,password,"fake salt");
         Assert.Equal("teste", accountObj.name.value);
         Assert.Equal("teste@", accountObj.username.value);
         Assert.Equal("teste@teste.com", accountObj.email.value);
-        Assert.NotNull(accountObj.password.value);
-        Assert.NotNull(accountObj.password.salt);
+        Assert.Equal("1122@xxX",accountObj.password.value);
     }
 }
