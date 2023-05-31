330ff2ae91e2999f35296afbebac548d86ae3f28
jsbeserra <joonassouza55@gmail.com>
2023-05-31 14:54:12 -0300

    feat: AccountRepositoryAdpterEntityFramework implements FindOne and FindOneByUsername

--- backend/auth/auth/infra/entityFramework-adpter/repository/account-repository-adpter-entityFramework.cs
+++ backend/auth/auth/infra/entityFramework-adpter/repository/account-repository-adpter-entityFramework.cs
@@ -11,10 +11,35 @@ public class AccountRepositoryAdpterEntityFramework:UsersRepository
             username = account.username.value,
             name = account.name.value,
             email = account.email.value,
-            password = account.password.value
+            password = account.password.value,
+            salt = account.salt,
         };
        this.dbcontext._AccountModel.Add(model);
        int affectedRows = await this.dbcontext.SaveChangesAsync();
        return affectedRows > 0;
+    }
+
+    public async Task<Account?> FindOne(string email)
+    {
+        var accountModel = this.dbcontext._AccountModel.Where(e=> e.email == email).FirstOrDefault();
+        if(accountModel == null) return null;
+        Name name = new Name(accountModel.name);
+        Username username = new Username(accountModel.username);
+        Email accountEmail = new Email(accountModel.email);
+        Password password = new Password(accountModel.password);
+        Account account = new Account(name,username,accountEmail,password,accountModel.salt);
+        return account;
+    }
+
+    public async Task<Account?> FindOneByUsername(string username)
+    {
+        var accountModel = this.dbcontext._AccountModel.Where(e=> e.username == username).FirstOrDefault();
+        if(accountModel == null) return null;
+        Name name = new Name(accountModel.name);
+        Username _username = new Username(accountModel.username);
+        Email accountEmail = new Email(accountModel.email);
+        Password password = new Password(accountModel.password);
+        Account account = new Account(name,_username,accountEmail,password,accountModel.salt);
+        return account;
     }
 }
