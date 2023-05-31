f06b70cd5b8cdea9ed29571942b325e73c2a2812
jsbeserra <joonassouza55@gmail.com>
2023-05-31 14:52:12 -0300

    feat: Account add salt

--- backend/auth/auth/domain/entities/account.cs
+++ backend/auth/auth/domain/entities/account.cs
@@ -3,12 +3,13 @@ public class Account {
     public Username username {get;}
     public Email email {get;}
     public Password password {get;}
-    public Account(Name name,Username username,Email email,Password password){
+
+    public string? salt {get;}
+    public Account(Name name,Username username,Email email,Password password,string salt){
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.salt = salt;
     }
-
-
 }
