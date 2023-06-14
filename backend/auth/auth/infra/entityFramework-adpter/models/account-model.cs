43b841b5df53bee32047a4dfdf62b38e5dc88dce
jsbeserra <joonassouza55@gmail.com>
2023-06-14 15:12:30 -0300

    feat: Accounts model add fields created_at and updated_at

--- backend/auth/auth/infra/entityFramework-adpter/models/account-model.cs
+++ backend/auth/auth/infra/entityFramework-adpter/models/account-model.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Accounts
 {
     public int id { get; set; }
@@ -6,5 +8,10 @@ public class Accounts
     public string email { get; set; } = "";
     public string password { get; set; } = "";
     public string salt { get; set; } = "";
+    [Column(TypeName = "timestamp with time zone")]
+    public DateTime? created_at { get; set; }
+    [Column(TypeName = "timestamp with time zone")]
+    public DateTime? updated_at { get; set; }
+
 
 }
